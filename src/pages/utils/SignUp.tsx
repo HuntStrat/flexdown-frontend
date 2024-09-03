693082a3b41a3ba362902e84c6e94d9e438f5ab5
Emmilija <karatashevskaemilija@gmail.com>
2024-09-04 01:57:03 +0200

    sign up page done

--- src/pages/utils/SignUp.tsx
+++ src/pages/utils/SignUp.tsx
@@ -6,9 +6,9 @@ const SignUp: React.FC = () => {
   return (
     <div className='h-screen p-8 flex justify-between items-center font-lexend'>
 
-<div className='w-1/2 h-full flex flex-col items-center justify-around'>
+<div className='w-full md:w-1/2 h-full flex flex-col items-center justify-around'>
 <div className='p-4 h-[80vh] md:w-[80%] flex flex-col justify-around'>
-<h1 className='lg:6xl font-bold text-center'>Sign Up</h1>
+<h1 className='text-3xl font-bold text-center'>Sign Up</h1>
 <span className='text-center'>Enter your email and password to register.</span>
 <div>
 <span>Your Email</span>
@@ -21,7 +21,7 @@ const SignUp: React.FC = () => {
 <div className='flex flex-col'>
 <button className='rounded-lg bg-black font-lexend text-white p-4 md:px-16 drop-shadow-xl md:mb-16'>REGISTER NOW</button>
 
-<div>
+<div className='mt-8'>
   <a href="">Already have an account? <span>Login</span> </a>
 </div>
 
@@ -33,10 +33,11 @@ const SignUp: React.FC = () => {
 
 </div>
 
-<div className='w-1/2'>
-  <img className='' src={Image} alt="" />
-</div>
-
+<div className="hidden w-0 md:block md:w-1/2">
+        <div className="p-4 bg-white ">
+          <img src={Image} alt="Login" className="w-full h-auto rounded-lg" />
+        </div>
+      </div>
     </div>
   );
 };
