555400c7b0472b9a000382ff63fa1653db17a3ef
Emmilija <karatashevskaemilija@gmail.com>
2024-09-03 16:14:56 +0200

    second and third part done for mobile and desktop

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { SiVorondesign } from "react-icons/si";
 import { BsGraphUpArrow } from "react-icons/bs";
 import { IoIosRocket } from "react-icons/io";
-import { useWindowResize } from '@/utils/windowResize';
+
 
 const Features: React.FC = () => {
-    const isMobile = useWindowResize();
+
   return (
-    <div className={`w-full h-auto flex ${isMobile ? 'flex-col' : 'flex-row'} pb-8 items-center justify-around bg-white font-lexend`}>
+    <div className={`w-full h-auto flex flex-col md:flex-row mt-8 pb-8 items-center justify-around bg-white font-lexend`}>
  
-      <div className={`flex ${isMobile ? 'w-[80%] pb-8' : 'w-[30%]'} flex-row items-start justify-between`}>
+      <div className={`flex w-[80%] pb-8 md:w-[30%] flex-row items-start justify-between`}>
         <div>
         <SiVorondesign className="text-2xl text-logo" />
         </div>
@@ -19,7 +19,7 @@ const Features: React.FC = () => {
         </div>
       </div>
 
-      <div className={`flex ${isMobile ? 'w-[80%] pb-8' : 'w-[30%]'} flex-row items-start justify-between`}>
+      <div className={`flex w-[80%] pb-8 md:w-[30%] flex-row items-start justify-between`}>
         <div>
         <BsGraphUpArrow className="text-2xl text-logo" />
         </div>
@@ -29,7 +29,7 @@ const Features: React.FC = () => {
         </div>
       </div>
 
-      <div className={`flex ${isMobile ? 'w-[80%] pb-8' : 'w-[30%]'} flex-row items-start justify-between`}>
+      <div className={`flex w-[80%] pb-8 md:w-[30%] flex-row items-start justify-between`}>
         <div>
         <IoIosRocket className="text-2xl text-logo"  />
         </div>
