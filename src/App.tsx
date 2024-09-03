9341cdf203e07c7e08ce556c260419c616c104cd
Emmilija <karatashevskaemilija@gmail.com>
2024-09-04 01:41:55 +0200

    login page done

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import '@fontsource/lexend';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../src/pages/Home'
 import SignUp from './pages/utils/SignUp';
+import Login from './pages/utils/Login';
 function App() {
 
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
       
         </Routes>
       </BrowserRouter>
