4efbff0e11e19f3bab1ec1d038f0b2fbd3e533f0
Emmilija <karatashevskaemilija@gmail.com>
2024-09-03 15:45:12 +0200

    fixed width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ const messages = [ 'Keep your sales team on top of every opportunity. FlexDown C
     
       <Navbar />
 
-      <div className='w-[90%]'>
+      <div className='w-[85%] md:w-[80%] px-2'>
       <div className="flex flex-col justify-start items-start text-greyy">
       <h1 className={`text-4xl md:text-5xl text-white font-bold mb-4`}>
     Connecting <br /><span className='animate-color-cycle'> You to Flexible</span><br /> Living Solutions
@@ -33,7 +33,7 @@ const messages = [ 'Keep your sales team on top of every opportunity. FlexDown C
   </div>
 
   <div className=' mt-8 h-[10vh]'>
-  <h2 className="text-2xl lg:text-3xl text-white font-lexend pr-16">
+  <h2 className="text-2xl lg:text-3xl text-white font-lexend pr-8">
      {text}
         </h2>
         <p className='text-white mt-2'>Flexible, Affordable, and Hassle-Free Living Solutions at Your Fingertips</p>
