845fe658ceecbb2b6e1a6160308242883f0d5625
Emmilija <karatashevskaemilija@gmail.com>
2024-09-09 15:01:22 +0200

    login done

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -4,57 +4,62 @@ import Image from '../../assets/images/signup.jpg';
 const Login: React.FC = () => {
   return (
     <div className="h-screen w-full p-8 md:flex justify-between items-center font-lexend">
-
-
-
-
       <div className="w-full md:w-1/2 h-full flex flex-col items-center justify-around">
         <div className="p-4 h-[90vh] md:w-[80%] flex flex-col justify-around">
           <h1 className="text-3xl font-bold text-center">Login</h1>
-          <span className="text-center">Enter your email and password to Login.</span>
-          <div>
-            <span>Your Email</span>
-            <input
-              className="w-full md:w-[100%] p-2 border-2 rounded-md focus:border-purple-500"
-              aria-label="Your Email"
-              type="text"
-            />
-          </div>
-
-          <div>
-            <span>Password</span>
-            <input
-              className="w-full md:w-[100%] p-2 border-2 rounded-md focus:border-purple-500"
-              aria-label="Password"
-              type="text"
-            />
-          </div>
-
-          <span>
-            <input type="checkbox" /> I agree with the{' '}
-            <a className="underline decoration-purple" href="">
-              Terms and Conditions
-            </a>
-          </span>
-
-          <div className="h-[30vh] flex flex-col justify-between">
-            <button className="rounded-lg bg-black font-lexend text-white p-4 md:px-16 drop-shadow-xl md:mb-0">
-              Sign Up
-            </button>
-
-            <div className="flex justify-between mt-8">
-              <span>
-                <input className="size-checkbox" type="checkbox" /> Subscribe me
-                for newsletters{' '}
-              </span>
-
-              <span>Forgot Password</span>
+          <span className="text-center">Enter your email and password to login.</span>
+          
+          <form>
+            <div className="mb-4">
+              <label className="block mb-2" htmlFor="email">Your Email</label>
+              <input
+                id="email"
+                className="w-full md:w-[100%] p-2 border-2 rounded-md focus:border-purple-500"
+                aria-label="Your Email"
+                type="email"
+                required
+              />
             </div>
 
-            <div className='text-center'>
-              <a className='text-purple' href="/signup">Not registered? <span className='text-black'>Create account</span></a>
+            <div className="mb-4">
+              <label className="block mb-2" htmlFor="password">Password</label>
+              <input
+                id="password"
+                className="w-full md:w-[100%] p-2 border-2 rounded-md focus:border-purple-500"
+                aria-label="Password"
+                type="password"
+                required
+              />
             </div>
-          </div>
+
+            <div className="mb-4">
+              <input className='mr-2' type="checkbox" id="terms" required />
+              <label htmlFor="terms">
+                I agree with the <a className="underline decoration-purple" href="">Terms and Conditions</a>
+              </label>
+            </div>
+
+            <div className="h-[30vh] flex flex-col justify-between">
+              <button type="submit" className="rounded-lg bg-black font-lexend text-white p-4 md:px-16 drop-shadow-xl">
+                Login
+              </button>
+
+              <div className="flex justify-between mt-8">
+                <span>
+                  <input className="size-checkbox" type="checkbox" id="subscribe" />
+                  <label htmlFor="subscribe"> Subscribe me for newsletters </label>
+                </span>
+
+                <a href="/forgot-password" className="underline">Forgot Password</a>
+              </div>
+
+              <div className="text-center">
+                <a className="text-purple" href="/signup">
+                  Not registered? <span className="text-black">Create account</span>
+                </a>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
 
@@ -63,7 +68,6 @@ const Login: React.FC = () => {
           <img src={Image} alt="Login" className="w-full h-auto rounded-lg" />
         </div>
       </div>
-    
     </div>
   );
 };
