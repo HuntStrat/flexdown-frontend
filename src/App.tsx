0606f4b7a8173f3df26cb88d2f8782fbf52580e2
Emmilija <karatashevskaemilija@gmail.com>
2024-09-10 15:27:10 +0200

    user form created

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import '@fontsource/lexend';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../src/pages/Home'
 import SignUp from './pages/auth/SignUp';
+import UserForm from './pages/UserForm';
 function App() {
 
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/userform" element={<UserForm />} />
       
         </Routes>
       </BrowserRouter>
