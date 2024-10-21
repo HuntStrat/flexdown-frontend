849d9425d449abc83b437278704413139da17602
Emmilija <karatashevskaemilija@gmail.com>
2024-10-21 22:32:25 +0200

    added chat

--- src/components/CreateUser.tsx
+++ src/components/CreateUser.tsx
@@ -1,14 +1,13 @@
-
 import React, { useEffect, useState } from 'react';
 import { Chat } from 'stream-chat-react';
 import { createUser } from '../utils/createUserChat';
+import Messenger from '../components/Messanger'; // Import Messenger
 
 const userId: string = 'delicate-pond-6';
 const userName: string = 'delicate';
 
 const CreateUser: React.FC = () => {
   const [isConnected, setIsConnected] = useState(false);
-
   const { client, user, userToken } = createUser(userId, userName);
 
   useEffect(() => {
@@ -16,64 +15,20 @@ const CreateUser: React.FC = () => {
       await client.connectUser(user, userToken);
       setIsConnected(true);
     };
-
+  
     connectUser();
-
+  
     return () => {
-      client.disconnectUser();
+      client.disconnectUser(); // Make sure this function is synchronous
     };
   }, [client, user, userToken]);
-
   if (!isConnected) return <div>Setting up client & connection...</div>;
 
   return (
     <Chat client={client}>
-      {/* Your chat components */}
-      Chat with client is ready!
+      <Messenger /> 
     </Chat>
   );
 };
 
 export default CreateUser;
-
-
-
-// src/components/CreateUser.tsx
-// import React, { useEffect, useState } from 'react';
-// import { Chat } from 'stream-chat-react';
-// import { createUser } from '../utils/createUser';
-
-// const userId: string = 'delicate-pond-6';
-// const userName: string = 'delicate';
-
-// const CreateUser: React.FC = () => {
-//   const [isConnected, setIsConnected] = useState(false);
-//   const [chatClient, setChatClient] = useState<any>(null);
-
-//   useEffect(() => {
-//     const initializeChat = async () => {
-//       const { client, user, userToken } = await createUser(userId, userName);
-
-//       await client.connectUser(user, userToken);
-//       setChatClient(client);
-//       setIsConnected(true);
-//     };
-
-//     initializeChat();
-
-//     return () => {
-//       if (chatClient) chatClient.disconnectUser();
-//     };
-//   }, []);
-
-//   if (!isConnected || !chatClient) return <div>Setting up client & connection...</div>;
-
-//   return (
-//     <Chat client={chatClient}>
-//       {/* Your chat components */}
-//       Chat with client is ready!
-//     </Chat>
-//   );
-// };
-
-// export default CreateUser;
---
+++ src/components/Messanger.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ChannelList, Channel, MessageList, MessageInput, Window } from 'stream-chat-react';
+
+const Messenger: React.FC = () => {
+  return (
+    <div className="flex h-full">
+    
+      <ChannelList />
+
+     
+      <Channel>
+        <Window>
+          <MessageList /> 
+          <MessageInput /> 
+        </Window>
+      </Channel>
+    </div>
+  );
+};
+
+export default Messenger;
